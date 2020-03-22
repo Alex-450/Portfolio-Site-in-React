00948ae255d1cdc702c2b98efd46381a9c3eb9f9
Alex-450 <alex.wmstearn@gmail.com>
2020-03-22 22:56:58 +0000

    Added show and hide functionality for blogs and refactored component

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
-import blogPostArchive from '../blogPostArchive';
+import blogPostArchive from '../blogPostArchive.json';
 import { Row, Col, Card, Button, Container, CardColumns } from 'react-bootstrap';
-import BlogPosts from './BlogPosts';
 
 class Blog extends React.Component {
     constructor(props) {
@@ -17,28 +16,31 @@ class Blog extends React.Component {
     loadBlogPost = () => {
         this.setState({postsLoaded: true});
     }
+
+    hideBlogPost = () => {
+        this.setState({postsLoaded: false})
+    }
         
     render() {
     return(
         <div>
             <NavBar />
-            <Container>
+            <Container className="title-container">
                 <Row>
                     <Col>
                         <Card>
                             <Card.Body>
                                 <Card.Title>{this.state.blogPosts.blog1.title}</Card.Title>
-                                <Card.Text>{this.state.postsLoaded && <BlogPosts />}</Card.Text>
-                                <Button onClick={this.loadBlogPost}>Load blog</Button>  
+                                <Card.Text>{this.state.postsLoaded && this.state.blogPosts.blog1.blogBody}</Card.Text>
+                                {!this.state.postsLoaded && <Button onClick={this.loadBlogPost} className="generic-button">Show blog ↓</Button>}
+                                {this.state.postsLoaded && <Button onClick={this.hideBlogPost} className="generic-button">Hide blog ↑</Button>}
                             </Card.Body>
                         <Card.Footer>{this.state.blogPosts.blog1.dateAdded}</Card.Footer>
                     </Card>
                 </Col>
             </Row>
-        
-            <Footer />
         </Container>
-
+        <Footer />
         </div>
     )
 }
