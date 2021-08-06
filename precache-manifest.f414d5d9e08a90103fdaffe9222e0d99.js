d018a76baed6ecc6928f2afcf7fa521d180de24f
Alex Stearn <alex.wmstearn@gmail.com>
2021-08-06 10:14:49 +0200

    Updates

---
+++ precache-manifest.f414d5d9e08a90103fdaffe9222e0d99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322f2f66a4a39e4fd5ff6e60ffa1adad",
+    "url": "/portfolio-site-in-react/index.html"
+  },
+  {
+    "revision": "45389458298a7cd644af",
+    "url": "/portfolio-site-in-react/static/css/2.84b3ebfc.chunk.css"
+  },
+  {
+    "revision": "12aa5180ad1843f5894a",
+    "url": "/portfolio-site-in-react/static/css/main.9ea8622a.chunk.css"
+  },
+  {
+    "revision": "45389458298a7cd644af",
+    "url": "/portfolio-site-in-react/static/js/2.3ab3bfbc.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio-site-in-react/static/js/2.3ab3bfbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12aa5180ad1843f5894a",
+    "url": "/portfolio-site-in-react/static/js/main.e5b0f4ca.chunk.js"
+  },
+  {
+    "revision": "2b3b1153872a28dc4713",
+    "url": "/portfolio-site-in-react/static/js/runtime-main.d6c5e405.js"
+  },
+  {
+    "revision": "a72cec2dbc92109ddb2629c93e2a4c9d",
+    "url": "/portfolio-site-in-react/static/media/blackPantherImage.a72cec2d.jpg"
+  },
+  {
+    "revision": "01aca6f88ece6709a4555cf5d714205c",
+    "url": "/portfolio-site-in-react/static/media/grandBudapest.01aca6f8.jpg"
+  }
+]);
