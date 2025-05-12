7c3941556a32eca4fd6231f2090406aa216542ce
Alex Stearn <alex.wmstearn@gmail.com>
2025-05-12 21:33:38 +0200

    remove old links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,16 +13,10 @@ const Footer = () => (
                 <Col xs={12} md={4}>
                     <Nav fixed="bottom" className="flex-column">
                         <Nav.Item>
-                            <Nav.Link className="footer-link" href="https://www.facebook.com/alex.stearn.5/" target="_blank" rel="noopener noreferrer">Facebook →</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
                             <Nav.Link className="footer-link" href="https://github.com/Alex-450" target="_blank" rel="noopener noreferrer">Github →</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
                             <Nav.Link className="footer-link" href="https://www.linkedin.com/in/alexander-stearn-96262914b/" target="_blank" rel="noopener">LinkedIn →</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link className="footer-link" href="https://twitter.com/AlexanderStearn" target="_blank" rel="noopener">Twitter →</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Col>
@@ -62,5 +56,5 @@ const Footer = () => (
         </Container>
     </div>
     );
-    
+
 export default Footer;
