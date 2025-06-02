fa3b65088565c8c8db1a72261bc3e9d9ac47ad20
Alex Stearn <alex.wmstearn@gmail.com>
2025-06-02 21:53:26 +0200

    fix typo

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,7 +15,7 @@ class About extends React.Component {
             <p>I grew up and went to school in Surrey and then went to university in Southampton.</p>
             <p>I studied History & German and have spent time living in Berlin and the Austrian alps. I worked in hospitality part-time from the age of 16, until I was 24. I have worked in all sorts of places, from <a target="_blank" rel="noopener noreferrer" href="https://7bone.co.uk/">burger joints</a> to cocktail bars.</p>
             <p>In 2018 I joined <a target="_blank" rel="noopener noreferrer" href="https://rekki.com/">REKKI</a>, a company providing technology for a sector I had grown to love: hospitality. I started working in the customer support team and after three months transitioned to the tech team where I ran the QA team.</p>
-            <p>In 2021 I joined <a target="_blank" rel="noopener noreferrer" href="https://fuga.com/">FUGA</a>, another company operating in a sector close to my heart: the music industry. This is where I started working as a software developer</p>
+            <p>In 2021 I joined <a target="_blank" rel="noopener noreferrer" href="https://fuga.com/">FUGA</a>, another company operating in a sector close to my heart: the music industry. This is where I started working as a software developer.</p>
             <p>Check out my education and work experience below:</p>
             <p></p>
             </Col>
