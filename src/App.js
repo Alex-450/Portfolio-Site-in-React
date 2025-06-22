bf14ea1dbc5109443b7ebc23fb4217b35ac6ba4f
Alex Stearn <alex.wmstearn@gmail.com>
2025-06-22 18:41:37 +0200

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import NavBar from './Components/NavBar';
 import Footer from './Components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Col, Row, Nav } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import './css/App.css'
-import { NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
