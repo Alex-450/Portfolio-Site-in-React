9e489566138d241008358eff56cee75b5cc65bbb
Alex Stearn <alex.wmstearn@gmail.com>
2025-06-28 12:59:54 +0200

    add metadata

--- src/Components/ArticleLayout.js
+++ src/Components/ArticleLayout.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useEffect } from "react";
 
-const ArticleLayout = ({ children }) => {
+const ArticleLayout = ({ metadata, children }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -15,6 +15,9 @@ const ArticleLayout = ({ children }) => {
         <Row className="justify-content-center">
           <Col xs={12} md={10} lg={8}>
             <article>
+              <title>{metadata.title}</title>
+              <meta name='author' content={metadata.author} />
+              <meta name='keywords' content={metadata.keywords} />
               {children}
             </article>
           </Col>
