b5dbb7cbec5bcde68a01a9a857392799a2fb6d16
Alex Stearn <alex.wmstearn@gmail.com>
2025-06-28 13:21:17 +0200

    update to react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './css/index.css';
 import * as serviceWorker from './serviceWorker';
 import Router from './Components/Router';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 
-render(<Router />, document.querySelector("#root"));
-
-
-
+import { createRoot } from 'react-dom/client';
+const container = document.querySelector("#root")
+const root = createRoot(container)
+root.render(<Router />)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
