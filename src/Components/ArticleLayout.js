060889f48c966eabab26cc206fc71f9ea231d84b
Alex Stearn <alex.wmstearn@gmail.com>
2025-07-08 10:50:11 +0200

    Add film name to meta og title

--- src/Components/ArticleLayout.js
+++ src/Components/ArticleLayout.js
@@ -15,7 +15,7 @@ const ArticleLayout = ({ metadata, children }) => {
             <meta name='description' content={metadata.description} property='og:description' />
             <meta name='author' content={metadata.author} />
             <meta name='keywords' content={metadata.keywords} />
-            <meta name='title' content={metadata.title} property="og:title" />
+            <meta name='title' content={`${metadata.filmTitle}: ${metadata.title}`} property="og:title" />
             <meta property="og:type" content="website" />
             <h1>{metadata.title}</h1>
             <p>{metadata.filmTitle} - {metadata.director} ({metadata.year})</p>
