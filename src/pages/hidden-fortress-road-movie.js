3e3dac299acfd8dbda4a61555eba9009114f09fd
Alex Stearn <alex.wmstearn@gmail.com>
2025-09-01 23:10:49 +0200

    Edits for Hidden Fortress

--- src/pages/hidden-fortress-road-movie.js
+++ src/pages/hidden-fortress-road-movie.js
@@ -21,23 +21,19 @@ const Page = () => {
       </p>
 
       <p>
-        Set against the backdrop of a civil war in medieval Japan, the protagonists of Akira Kurosawa’s <i>The Hidden Fortress</i> are never far away from death. We are introduced to the two hapless, greedy, scheming peasants Tahei and Matashichi as they attempt to escape Akizuki territory that has just been occupied by the neighbouring Yamana clan. They had left the peace of their home in Hayakawa with the hopes of making a fortune in the war, but had arrived too late and been forced to dig graves by the occupying army. In the opening sequence they witness a fleeing soldier being chased down and killed. Shortly afterwards Tahei sees another soldier shot whilst attempting to cross the newly closed and heavily guarded border between Akizuki and Hayakawa.
+        Set against the backdrop of a civil war in medieval Japan, the protagonists of Akira Kurosawa’s <i>The Hidden Fortress</i> are never far away from death. We are introduced to the two hapless, greedy, scheming peasants Tahei and Matashichi as they attempt to escape occupied territory. They had left the peace of their home with the hopes of making a fortune in the war, but had arrived too late and had been forced to dig graves instead. In the opening sequence they witness a fleeing soldier being chased down and killed. Shortly afterwards Tahei sees another soldier shot whilst attempting to cross the newly closed and heavily guarded border.
       </p>
 
       <p>
-        Later on, they stumble across a piece of gold hidden in a stick and are discovered by the Akizuki General Rokurota, who has managed to escape the Yamana with the Princess Yuki and a large stash of gold, and is planning to escort her to the safety of Hayakawa. The general initially considers killing them, but when he learns of their plan to get home to Hayakawa by sneaking into Yamana territory and then back over the less closely guarded border between Yamana and Hayakawa, he decides to play on their greed and offer them a share of the gold in return for their help.
+        Whilst plotting their escape they run into General Rokurota who is also on the run from the occupying forces, with a large stash of gold smuggled out of the castle and the Princess Yuki, who is also fleeing for her life. The general initially considers killing them, but when he learns of their unorthodox plan to get across the border he decides to play on their greed and offer them a share of the gold in return for their help.
       </p>
 
       <p>
-        The adventure that follows, sneaking behind enemy lines, constantly on the verge of capture and certain death is filled with suspense, comedy and a gradual changing of perspective for the Princess Yuki. She realises that living in the castle up until this point has left her isolated and out of touch, she doesn’t know the people that she has ruled. Despite Tahei and Matashichi’s almost constant attempts to betray both her and Rokurota and total obsession with finding fortune, she doesn’t seem to be scared of or disgusted by them. She bats them away and outwits them with seeming ease. She finds them entertaining. She has not encountered people who treat her like this in her sheltered upbringing, and they contrast totally with Rokurota, whose undying loyalty (to the point of sacrificing his own sister, Kofuyu, as a decoy) she utterly detests:
+        The adventure that follows, sneaking behind enemy lines, constantly on the verge of capture and certain death is filled with suspense, comedy and a gradual changing of perspective for the Princess Yuki. She realises that living in the castle has left her isolated and out of touch, she doesn’t know the people that she has ruled. Despite Tahei and Matashichi’s almost constant attempts to betray both her and Rokurota and total obsession with getting rich, she doesn’t seem to be scared of or disgusted by them. She bats them away and outwits them with seeming ease. She finds them entertaining. She has not encountered people who treat her like this in her sheltered upbringing, and they contrast totally with Rokurota, whose undying loyalty she utterly detests.
       </p>
 
       <p>
-        <i>“Kofuyu was 16… I am 16. What difference is there in our souls?… If I was Kofuyu, I would curse this princess”</i>
-      </p>
-
-      <p>
-        In contrast the two peasants would sell her out for a piece of gold in a heartbeat. They attempt to do exactly this as soon as they meet her, but are thwarted by Kofuyu being executed in her stead - leading them to believe that the princess is already dead. She seems comfortable in their company because they don’t believe that she is a princess and don’t treat her as one.
+        In contrast the two peasants would sell her out for a piece of gold in a heartbeat. They attempt to do exactly this as soon as they meet her. She seems comfortable in their company because they don’t see her as deserving of special treatment.
       </p>
 
       <p>
@@ -45,7 +41,7 @@ const Page = () => {
       </p>
 
       <p>
-        When she is eventually captured and is facing execution, she realises that the richness of life might have passed her by had she spent all of her time in the castle. Her proximity to death has let her experience life more fully and see both the darkness and the light. She has been able to interact with people as if she were on their level, not as a princess to her subjects. She has proven herself capable not just of living in the real world, but of thriving.
+        When she is eventually captured and is facing execution, she realises that the richness of life might have passed her by had she spent all of her time in the castle. Her proximity to death has let her experience life more fully and allowed her to see both the darkness and the light. She has been able to interact with people on their level, not as a princess to her subjects. She has proven herself capable not just of living in the real world, but of thriving.
       </p>
 
       <p>
