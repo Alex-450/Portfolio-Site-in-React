bc0617401be957abd3511dcf44e7a18e77355918
Alex Stearn <alex.wmstearn@gmail.com>
2025-09-08 22:44:50 +0200

    fomat lastModified as date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,10 +5,16 @@ export const dynamic = "force-static"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://a-450.com'
-  const blogPages = blogPostArchive.flatMap((blog) => ({
-    url: `${baseUrl}/${blog.link}`,
-    lastModified: blog.dateAdded,
-  }))
+
+  const blogPages = blogPostArchive.flatMap((blog) => {
+    const [day, month, year] = blog.dateAdded.split('/').map(Number)
+    const date = new Date(Date.UTC(year, month - 1, day))
+
+    return {
+      url: `${baseUrl}${blog.link}`,
+      lastModified: date.toISOString(),
+    }
+  })
 
   return [
     { url: baseUrl, lastModified: new Date().toISOString() }, // homepage
