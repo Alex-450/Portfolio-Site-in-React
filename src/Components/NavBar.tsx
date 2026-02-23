24f4c733fe70fb9e2bdb91a37fd0f31a9c64870d
Alex Stearn <alex.wmstearn@gmail.com>
2026-02-23 22:11:09 +0100

    simplify nav

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -8,57 +8,28 @@ import DarkModeToggle from './DarkModeToggle';
 import '../css/index.css';
 
 const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const pathname = usePathname();
-
-  // Close menu when route changes
-  useEffect(() => {
-    setMenuOpen(false);
-  }, [pathname]);
-
-  // Close menu when clicking outside (for mobile)
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setMenuOpen(false);
-      }
-    };
-
-    if (menuOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [menuOpen]);
-
   return (
     <div className="navbar-container">
       <Container>
-        <Navbar className="navbar" variant="dark" sticky="top" expand="lg">
-          <Col md={10} className="header-menu-wrapper" ref={menuRef}>
+        <Navbar className="navbar" variant="dark" sticky="top">
+          <Col md={10} className="header-menu-wrapper">
             <span
               className="navbar-brand header-menu-trigger"
-              onClick={() => setMenuOpen(!menuOpen)}
             >
               a-450
             </span>
             <div
-              className={`header-menu-dropdown ${menuOpen ? 'menu-open' : ''}`}
+              className="header-menu-dropdown"
             >
               <Link
                 href="/"
                 className="header-menu-link"
-                onClick={() => setMenuOpen(false)}
               >
                 Blog ›
               </Link>
               <Link
                 href="/film-listings"
                 className="header-menu-link"
-                onClick={() => setMenuOpen(false)}
               >
                 Film Listings ›
               </Link>
