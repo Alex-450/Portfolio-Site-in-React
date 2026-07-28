78295c9389e3517f923f6fd57405e09ee45a56ee
Alex Stearn <alex.wmstearn@gmail.com>
2026-07-28 11:34:15 +0200

    add experimental.useTypeScriptCli

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   trailingSlash: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    useTypeScriptCli: true, // TODO: revisit once Typescript 7 is properly supported
+  },
 };
 
 const withMDX = createMDX();
